094cd676ba5cdfe892b9937e2715c06ccbf6f986
christyan <chrisaraujo124@gmail.com>
2022-07-28 11:13:57 -0300

    gera senhas

---
+++ script.js
@@ -0,0 +1,40 @@
+let sliderElement = document.querySelector("#slider");
+let buttonElement = document.querySelector("#button");
+
+let sizePassword = document.querySelector("#valor");
+let password = document.querySelector("#password");
+
+let containerPassword = document.querySelector("#container-password");
+
+let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@";
+let novaSenha = "";
+
+
+sizePassword.innerHTML = sliderElement.value;                   //pego o valor que o usuario quer
+slider.oninput = function()
+{
+    sizePassword.innerHTML = this.value;
+}
+
+function generatePassword()
+{
+    //alert("A senha sera gerada!")
+    
+    let pass = "";
+
+    for(let i = 0, n = charset.length; i < sliderElement.value; ++i)
+    {
+        pass += charset.charAt(Math.floor(Math.random() * n));
+    }
+
+    containerPassword.classList.remove("hide");
+    password.innerHTML = pass;
+    novaSenha = pass;
+}
+
+function copyPassword()
+{
+    //alert("Senha copiada com sucesso!")
+
+    navigator.clipboard.writeText(novaSenha);
+}
